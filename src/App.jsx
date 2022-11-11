a5941df51ada02f65ef72d6fd4b634406ed0d11e
Rodrigo <rodrigo.facury14@gmail.com>
2022-11-11 16:04:57 -0300

    feat: cria botão para random page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Calculator from './Pages/Calculator/Calculator';
 import Information from './Pages/Information/Information';
 import './App.css';
+import RandomPage from './Pages/RandomPage';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Calculator />} />
         <Route path='/saiba-mais' element={<Information />} />
+        <Route path='/button' element={<RandomPage />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/Pages/RandomPage.jsx
@@ -0,0 +1,11 @@
+import Button from '../Components/Button';
+
+function RandomPage() {
+  return (
+    <Button>
+      Texto do botão
+    </Button>
+  );
+}
+
+export default RandomPage;
