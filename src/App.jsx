05e605b19e30e03f83325ea847f442c19c21f5db
Rodrigo <rodrigo.facury14@gmail.com>
2022-11-16 16:54:49 -0300

    fix: remove página inutilizada

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Calculator from './Pages/Calculator/Calculator';
 import Information from './Pages/Information/Information';
 import './App.css';
-import RandomPage from './Pages/RandomPage';
 import { Provider } from 'react-redux';
 import { store } from './store.js';
 
@@ -13,7 +12,6 @@ function App() {
         <Routes>
           <Route path='/' element={<Calculator />} />
           <Route path='/saiba-mais' element={<Information />} />
-          <Route path='/button' element={<RandomPage />} />
         </Routes>
       </BrowserRouter>
     </Provider>
