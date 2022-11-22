11bda1a804833d1355ce022ce78c1efa42ff143c
Rodrigo <rodrigo.facury14@gmail.com>
2022-11-22 19:58:59 -0300

    feat: adiciona página não encontrada

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Calculator from './Pages/Calculator/Calculator';
 import Information from './Pages/Information/Information';
+import NotFound from './Pages/NotFound';
 import './App.css';
 import { Provider } from 'react-redux';
 import { store } from './store.js';
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Calculator />} />
           <Route path='/saiba-mais' element={<Information />} />
+          <Route path='*' element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </Provider>
